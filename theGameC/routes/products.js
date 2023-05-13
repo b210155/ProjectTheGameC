7afee9301d7f16db2c48bea729dd6232c848b6b3
LAPTOP-GHBD2O7D\黃子洋 <b210155@gmail.com>
2023-05-14 05:46:18 +0800

    商品單頁

--- theGameC/routes/products.js
+++ theGameC/routes/products.js
@@ -2,8 +2,14 @@ const express = require("express");
 const page = express.Router();
 const path = require("path");
 
+/* 購物車頁 */
 page.get("/shopping_cart", (req, res) => {
   res.render("shopping_cart");
+});
+
+/* 商品單頁 */
+page.get("/product_Page", (req, res) => {
+  res.render("productPage");
 });
 
 module.exports = page;
