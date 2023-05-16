75999df34b1fa91c1a920daf86a3d42168bca2b2
DESKTOP-8D7LR2I\user <b210155@gmail.com>
2023-05-16 16:27:47 +0800

    遊戲單頁

--- theGameC/routes/game_CRUD/g_select .js
+++ theGameC/routes/game_CRUD/g_select .js
@@ -4,35 +4,48 @@ const page = express.Router();
 const config = require("../CRUD/config"); // 引用 config
 
 //中間區塊要抓遊戲種類圖片 資料庫：games
-page.get('/game_select', (req, res) => {
+page.get("/game_select", (req, res) => {
   var sql = "SELECT game_type , image ,game_name,age_rating FROM games;";
   config.query(sql, function (err, results, fields) {
     if (err) {
-      res.status(500).json({error:'selec錯誤',details:err});
-    }else{
-      console.log('查詢結果：',results[0]);
+      res.status(500).json({ error: "selec錯誤", details: err });
+    } else {
+      console.log("查詢結果：", results[0]);
       res.send(JSON.stringify(results));
     }
-    
-    
   });
 });
-    
- 
+
 //下面區塊要抓遊戲背景圖 資料庫：game_type
-page.get('/game_select_bg', (req, res) => {
+page.get("/game_select_bg", (req, res) => {
   var sql = "SELECT game_type_name,description, bg_image FROM game_types;";
   config.query(sql, function (err, results, fields) {
     if (err) {
       console.error(err);
-      res.status(500).send('服務器有問題');
+      res.status(500).send("服務器有問題");
       return;
     }
-    console.log('查詢結果：',results[0]);
+    console.log("查詢結果：", results[0]);
     res.send(JSON.stringify(results));
-    
-    
   });
+});
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+/* 遊戲單頁 */
+page.get("/api/gameID/:game_id", (req, res) => {
+  var sql =
+    "SELECT *, DATE_FORMAT(created_at, '%Y年-%m月-%d日') AS formatted_created_at FROM `games` WHERE game_id = ?;";
+  config.query(
+    sql,
+    [req.params.game_id], // 名稱照 /: 打
+    function (err, results, fields) {
+      console.log(results[0]);
+      res.send(JSON.stringify(results[0]));
+    }
+  );
 });
 
 module.exports = page;
--- theGameC/routes/games.js
+++ theGameC/routes/games.js
@@ -25,19 +25,64 @@ page.get("/", async (req, res) => {
   }
 });
 
-// page.get('/data1', async (req, res) => {
-// try {
-// const response = await axios.get('http://localhost:80/games/game_select_bg'); // 向g_select.js裡面撈資料
-// res.render('games',          // 渲染 games.ejs
-// { cat:response.data });
-// console.log('下方背景圖片資料有收到');
-// } catch (error) {
-// console.error(error);
-// res.status(500).send('下方背景圖片服務器有问题');
-// console.log('下方背景圖片沒收到');
-// }
-// });
-//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* 遊戲單頁 */
+page.get("/gamepage/:game_id", async (req, res) => {
+  let gameSelect = await axios.get(
+    `http://localhost:80/games/api/gameID/${req.params.game_id}`
+  );
+  // 類型 Icon、CSS背景、你可能喜歡 SELECT 路徑
+  var typeIcon = "";
+  var typeCss = "";
+  var typeName = ""; // 你可能會喜歡 SELECT 路徑
+  switch (gameSelect.data.game_type) {
+    case "射擊遊戲":
+      typeIcon = "/img/Games/GameTypeBg/typeIShooting.png";
+      typeCss = "ProductPageBgShooting";
+      typeName = gameSelect.data.game_type;
+      break;
+    case "冒險遊戲":
+      typeIcon = "/img/Games/GameTypeBg/typeIAVG.png";
+      typeCss = "ProductPageBgAVG";
+      typeName = gameSelect.data.game_type;
+      break;
+    case "競技遊戲":
+      typeIcon = "/img/Games/GameTypeBg/typeICom.png";
+      typeCss = "ProductPageBgCom";
+      typeName = gameSelect.data.game_type;
+      break;
+  }
+  // 年齡限制
+  var ageRating = "";
+  switch (gameSelect.data.age_rating) {
+    case "6":
+      ageRating = "保護級";
+      break;
+    case "12":
+      ageRating = "輔導級";
+      break;
+    case "18":
+      ageRating = "限制級";
+      break;
+    default:
+      ageRating = "普遍級";
+      break;
+  }
+
+  console.log(gameSelect.data.game_type);
+  res.render(
+    "gamepage", // 渲染 games.ejs
+    {
+      typeIcon: typeIcon,
+      typeCss: typeCss,
+      ageRating: ageRating,
+      gameData: gameSelect.data,
+    }
+  );
+});
 
 page.use("/", game_CRUD_select);
 
