1e31c424794ee187bb18b1045ef4a9b0af1bdce2
DESKTOP-8D7LR2I\user <b210155@gmail.com>
2023-05-16 10:03:24 +0800

    onlogin 修改

--- theGameC/SQL/shopping_carts.sql
+++ theGameC/SQL/shopping_carts.sql
@@ -4,7 +4,7 @@ CREATE TABLE shopping_carts ( -- 購物車資料表(一欄表示一個商品)
   product_id INT NOT NULL, -- 購物車內的商品(一欄代表一個商品)
   price INT UNSIGNED NOT NULL, -- 購物車內單項商品價格
   FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (product_id) REFERENCES products(product_id)
+  FOREIGN KEY (product_id) REFERENCES products(product_id),
   UNIQUE (user_id, product_id) -- 一個商品只能被一個使用者放入購物車一次
 );
 
